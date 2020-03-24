15639991578bb30609acd98beaf868ff90826b84
hh520 <lijiahao3306@126.com>
2020-03-24 16:39:55 +0800

    first commit

---
+++ src/main/java/com/swagger/main/Application.java
@@ -0,0 +1,14 @@
+package com.swagger.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.swagger.main"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
